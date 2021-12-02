18395293b4c395dbd0db783d0c8e831db86d5452
Mikhail Moysenko <moysenko@yandex-team.ru>
2021-12-02 23:51:27 +0300

    minor changes

--- app/songs/urls.py
+++ app/songs/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import SongPage, SongList, index
 
 urlpatterns = [
-    path('react', index),
     path('', SongList.as_view(), name="song_list"),
     path('<slug:pk>', SongPage.as_view(), name="song_page")
 ]
--- app/songs/views.py
+++ app/songs/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from .models import Song
 from django.views.generic import ListView, DetailView
 
-def index(request):
-  return render(request, 'songs/react.html')
-
 class SongList(ListView):
   template_name = 'songs/songList.html'
   model = Song
